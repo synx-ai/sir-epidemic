db57e1f9f928a464c8955a325d5effc06728a78c
Carlos Rivera <carlos@synx.co>
2020-01-28 06:28:04 -0600

    fixed relative path

--- epidemic/config/index.js
+++ epidemic/config/index.js
@@ -43,7 +43,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../../docs'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/regex',
+    assetsPublicPath: '/sir-epidemic',
 
     /**
      * Source Maps
